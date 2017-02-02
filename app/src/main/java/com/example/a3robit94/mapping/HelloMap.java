15e7dea2e7125ab3668ab28b74d7cee459f15822
3robit94 <3robit94@solent.ac.uk>
2017-02-02 10:42:00 +0000

    Added search by latitude and longitude

--- app/src/main/java/com/example/a3robit94/mapping/HelloMap.java
+++ app/src/main/java/com/example/a3robit94/mapping/HelloMap.java
@@ -4,12 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
 
-public class HelloMap extends Activity{
+public class HelloMap extends Activity implements View.OnClickListener {
     //creates an activity through the use of extends
 
     MapView mv;
@@ -27,9 +30,19 @@ public class HelloMap extends Activity{
 
         mv = (MapView)findViewById(R.id.map1); //finds the mapview by ID
 
-
         mv.setBuiltInZoomControls(true); //Gives zoom controls
         mv.getController().setZoom(14); //Sets zoom level
-        mv.getController().setCenter(new GeoPoint(40.1,22.5)); //Sets the position of the map
+        mv.getController().setCenter(new GeoPoint(40.1,22.5)); //Sets the position of the map via longtitude/latitude
+
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+    }
+
+    public void onClick(View MapView){
+        EditText longitudeEditText = (EditText) findViewById(R.id.longitudeEditText);
+        double longitude = Double.parseDouble(longitudeEditText.getText().toString());
+        EditText latitudeEditText = (EditText) findViewById(R.id.latitudeEditText);
+        double latitude = Double.parseDouble(latitudeEditText.getText().toString());
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
     }
 }
